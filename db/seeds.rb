6e04a5d204e21153ff3dc65652a1841ff908258a
Valentin Fontana <valenfontana7@gmail.com>
2020-11-20 12:19:58 -0300

    seed2

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,22 @@ user = User.create(
 )
 
 Article.create([
-                 { title: 'Article title 1', content: 'Article content 1', slug: 'article-title-1' },
-                 { title: 'Article title 2', content: 'Article content 2', slug: 'article-title-2' },
-                 { title: 'Article title 3', content: 'Article content 3', slug: 'article-title-3' }
+                 {
+                   title: 'Article title 1',
+                   content: 'Article content 1',
+                   slug: 'article-title-1',
+                   user: user
+                 },
+                 {
+                   title: 'Article title 2',
+                   content: 'Article content 2',
+                   slug: 'article-title-2',
+                   user: user
+                 },
+                 {
+                   title: 'Article title 3',
+                   content: 'Article content 3',
+                   slug: 'article-title-3',
+                   user: user
+                 }
                ])
