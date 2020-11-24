31a75e5c75bcf1666e1eb29fc65d921cb73fdba3
Valentin Fontana <valenfontana7@gmail.com>
2020-11-24 10:47:19 -0300

    asd

---
+++ app/lib/user_authenticator/standard.rb
@@ -0,0 +1,9 @@
+class UserAuthenticator::Standard < UserAuthenticator
+  class AuthenticationError < StandardError; end
+
+  def initialize(login: nil, password: nil); end
+
+  def perform
+    raise AuthenticationError
+  end
+end
